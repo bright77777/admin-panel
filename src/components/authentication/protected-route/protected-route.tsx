da8510856a99a77debcbe99803b0c49056767262
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-19 15:18:01 +0200

    eslint/prettier - fix components/authentication/protected-route

--- src/components/authentication/protected-route/protected-route.tsx
+++ src/components/authentication/protected-route/protected-route.tsx
@@ -1,23 +1,26 @@
-import { Spinner } from "@medusajs/icons"
-import { Navigate, Outlet, useLocation } from "react-router-dom"
-import { useMe } from "../../../hooks/api/users"
-import { SearchProvider } from "../../../providers/search-provider"
-import { SidebarProvider } from "../../../providers/sidebar-provider"
+import { Spinner } from "@medusajs/icons";
+
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+import { useMe } from "@hooks/api";
+
+import { SearchProvider } from "@providers/search-provider";
+import { SidebarProvider } from "@providers/sidebar-provider";
 
 export const ProtectedRoute = () => {
-  const { user, isLoading } = useMe()
-  const location = useLocation()
+  const { user, isLoading } = useMe();
+  const location = useLocation();
 
   if (isLoading) {
     return (
       <div className="flex min-h-screen items-center justify-center">
-        <Spinner className="text-ui-fg-interactive animate-spin" />
+        <Spinner className="animate-spin text-ui-fg-interactive" />
       </div>
-    )
+    );
   }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return (
@@ -26,5 +29,5 @@ export const ProtectedRoute = () => {
         <Outlet />
       </SearchProvider>
     </SidebarProvider>
-  )
-}
+  );
+};
