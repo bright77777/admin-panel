c1a97e17d96dec72b444e76c43d8716ae9060cf5
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-19 17:01:02 +0200

    eslint/prettier - fix components/data-grid/context

--- src/components/data-grid/context/data-grid-context.tsx
+++ src/components/data-grid/context/data-grid-context.tsx
@@ -1,45 +1,54 @@
-import { FocusEvent, MouseEvent, createContext } from "react"
-import {
+import type { FocusEvent, MouseEvent } from "react";
+import { createContext } from "react";
+
+import type {
   Control,
   FieldErrors,
   FieldValues,
   Path,
   UseFormRegister,
-} from "react-hook-form"
-import { CellErrorMetadata, CellMetadata, DataGridCoordinates } from "../types"
+} from "react-hook-form";
+
+import type {
+  CellErrorMetadata,
+  CellMetadata,
+  DataGridCoordinates,
+} from "@components/data-grid/types";
 
 type DataGridContextType<TFieldValues extends FieldValues> = {
   // Grid state
-  anchor: DataGridCoordinates | null
-  trapActive: boolean
-  setTrapActive: (value: boolean) => void
-  errors: FieldErrors<TFieldValues>
+  anchor: DataGridCoordinates | null;
+  trapActive: boolean;
+  setTrapActive: (value: boolean) => void;
+  errors: FieldErrors<TFieldValues>;
   // Cell handlers
-  getIsCellSelected: (coords: DataGridCoordinates) => boolean
-  getIsCellDragSelected: (coords: DataGridCoordinates) => boolean
+  getIsCellSelected: (coords: DataGridCoordinates) => boolean;
+  getIsCellDragSelected: (coords: DataGridCoordinates) => boolean;
   // Grid handlers
-  setIsEditing: (value: boolean) => void
-  setIsSelecting: (value: boolean) => void
-  setRangeEnd: (coords: DataGridCoordinates) => void
-  setSingleRange: (coords: DataGridCoordinates) => void
+  setIsEditing: (value: boolean) => void;
+  setIsSelecting: (value: boolean) => void;
+  setRangeEnd: (coords: DataGridCoordinates) => void;
+  setSingleRange: (coords: DataGridCoordinates) => void;
   // Form state and handlers
-  register: UseFormRegister<TFieldValues>
-  control: Control<TFieldValues>
+  register: UseFormRegister<TFieldValues>;
+  control: Control<TFieldValues>;
   getInputChangeHandler: (
-    field: Path<TFieldValues>
-  ) => (next: any, prev: any) => void
+    field: Path<TFieldValues>,
+    //@todo fix type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ) => (next: any, prev: any) => void;
   // Wrapper handlers
   getWrapperFocusHandler: (
-    coordinates: DataGridCoordinates
-  ) => (e: FocusEvent<HTMLElement>) => void
+    coordinates: DataGridCoordinates,
+  ) => (e: FocusEvent<HTMLElement>) => void;
   getWrapperMouseOverHandler: (
-    coordinates: DataGridCoordinates
-  ) => ((e: MouseEvent<HTMLElement>) => void) | undefined
-  getCellMetadata: (coords: DataGridCoordinates) => CellMetadata
-  getCellErrorMetadata: (coords: DataGridCoordinates) => CellErrorMetadata
-  navigateToField: (field: string) => void
-}
+    coordinates: DataGridCoordinates,
+  ) => ((e: MouseEvent<HTMLElement>) => void) | undefined;
+  getCellMetadata: (coords: DataGridCoordinates) => CellMetadata;
+  getCellErrorMetadata: (coords: DataGridCoordinates) => CellErrorMetadata;
+  navigateToField: (field: string) => void;
+};
 
 export const DataGridContext = createContext<DataGridContextType<any> | null>(
-  null
-)
+  null,
+);
