f3461bf848405fe963e307c3cb9d06b98c8b658b
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-21 10:43:40 +0200

    eslint/prettier - fix components/table/table-cells/product/variant-cell

--- src/components/table/table-cells/product/variant-cell/variant-cell.tsx
+++ src/components/table/table-cells/product/variant-cell/variant-cell.tsx
@@ -1,17 +1,18 @@
-import { useTranslation } from "react-i18next"
+import type { HttpTypes } from "@medusajs/types";
 
-import { PlaceholderCell } from "../../common/placeholder-cell"
-import { HttpTypes } from "@medusajs/types"
+import { useTranslation } from "react-i18next";
+
+import { PlaceholderCell } from "@components/table/table-cells/common/placeholder-cell";
 
 type VariantCellProps = {
-  variants?: HttpTypes.AdminProductVariant[] | null
-}
+  variants?: HttpTypes.AdminProductVariant[] | null;
+};
 
 export const VariantCell = ({ variants }: VariantCellProps) => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   if (!variants || !variants.length) {
-    return <PlaceholderCell />
+    return <PlaceholderCell />;
   }
 
   return (
@@ -20,15 +21,15 @@ export const VariantCell = ({ variants }: VariantCellProps) => {
         {t("products.variantCount", { count: variants.length })}
       </span>
     </div>
-  )
-}
+  );
+};
 
 export const VariantHeader = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="flex h-full w-full items-center">
       <span>{t("fields.variants")}</span>
     </div>
-  )
-}
+  );
+};
