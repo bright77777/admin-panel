10acbfe4812cbbdba4f7f27370c02e118ee02803
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-21 11:41:00 +0200

    eslint/prettier - fix components/utilities/i18n

--- src/components/utilities/i18n/i18n.tsx
+++ src/components/utilities/i18n/i18n.tsx
@@ -1,12 +1,12 @@
-import i18n from "i18next"
-import LanguageDetector from "i18next-browser-languagedetector"
-import { initReactI18next } from "react-i18next"
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-import { defaultI18nOptions } from "../../../i18n/config"
+import { defaultI18nOptions } from "@/i18n/config.ts";
 
 export const I18n = () => {
   if (i18n.isInitialized) {
-    return null
+    return null;
   }
 
   i18n
@@ -14,12 +14,12 @@ export const I18n = () => {
       new LanguageDetector(null, {
         lookupCookie: "lng",
         lookupLocalStorage: "lng",
-      })
+      }),
     )
     .use(initReactI18next)
-    .init(defaultI18nOptions)
+    .init(defaultI18nOptions);
 
-  return null
-}
+  return null;
+};
 
-export { i18n }
+export { i18n };
