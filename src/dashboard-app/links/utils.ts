e0d65c11f7ac3e5f60cb6530aefb2412f306ec60
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-21 15:04:25 +0200

    eslint/prettier - fix dashboard/links

--- src/dashboard-app/links/utils.ts
+++ src/dashboard-app/links/utils.ts
@@ -1,20 +1,22 @@
-import { CustomFieldModel } from "@medusajs/admin-shared"
-import linkModule from "virtual:medusa/links"
+import type { CustomFieldModel } from "@medusajs/admin-shared";
+
+import linkModule from "virtual:medusa/links";
 
 function appendLinkableFields(
   fields: string = "",
-  linkable: (string | string[])[] = []
+  linkable: (string | string[])[] = [],
 ) {
   const linkableFields = linkable.flatMap((link) => {
     return typeof link === "string"
       ? [`+${link}.*`]
-      : link.map((l) => `+${l}.*`)
-  })
+      : link.map((l) => `+${l}.*`);
+  });
 
-  return [fields, ...linkableFields].join(",")
+  return [fields, ...linkableFields].join(",");
 }
 
 export function getLinkedFields(model: CustomFieldModel, fields: string = "") {
-  const links = linkModule.links[model]
-  return appendLinkableFields(fields, links)
+  const links = linkModule.links[model];
+
+  return appendLinkableFields(fields, links);
 }
